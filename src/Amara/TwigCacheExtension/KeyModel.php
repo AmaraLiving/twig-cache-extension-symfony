9657b34398595d6da76c69c62419f696719df321
Ross Motley <ross.motley@gmail.com>
2017-01-16 20:41:29 +0000

    Added MIT license

--- src/Amara/TwigCacheExtension/KeyModel.php
+++ src/Amara/TwigCacheExtension/KeyModel.php
@@ -1,6 +1,12 @@
 <?php
-/**
+
+/*
+ * This file is part of the twig-cache-extension-symfony package.
  *
+ * (c) Amara Living Ltd
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Amara\TwigCacheExtension;
